46dd09fc42e04cbef8a554ee079a7bfb6f59e280
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-08 16:47:43 +0500

    MasterSettings.jsx process

--- src/pages/Master/Settings/MasterSettings.jsx
+++ src/pages/Master/Settings/MasterSettings.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import toast from "react-hot-toast";
 
 // img
 import logo from "../../../../public/img/logo.png";
 import EditUserInfo from "./EditUserInfo";
 import EditCardInfo from "./EditCardInfo";
 import AddCard from "./AddCard";
+
+// http
+import http from "../../../services/http";
 
 function MasterSettings() {
   const [smlist, setSmlist] = useState("profile");
@@ -14,6 +19,34 @@ function MasterSettings() {
     setSmlist(e);
   };
   // Change Staff/Master list END
+
+  const [personalInfo, setPersonalInfo] = useState([]);
+
+  const getPersonalInfo = () => {
+    const userId = localStorage.getItem("userId");
+    toast.promise(
+      http.get(`users/staff/${userId}/`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("access")}` },
+      }),
+      {
+        loading: "Loading ...",
+        success: (response) => {
+          console.log(response.data);
+          setPersonalInfo(response.data);
+          return <b>Success :)</b>;
+        },
+        error: (error) => {
+          console.log(error.response.data);
+
+          return <b>Error :(</b>;
+        },
+      }
+    );
+  };
+
+  useEffect(() => {
+    getPersonalInfo();
+  }, []);
 
   return (
     <>
@@ -88,13 +121,13 @@ function MasterSettings() {
         </div>
 
         <div>
-          <div className="flex border-[2px] border-custom-green-60 rounded-[5px] gap-4 mt-[25px] p-3 items-center">
+          <div className="flex border-[2px] border-custom-green-15 rounded-[5px] gap-4 mt-[25px] p-3 items-center">
             <div className="flex justify-center items-center w-[80px] h-[80px]">
               {/* <i className="bi bi-person-bounding-box text-[35px]"></i> */}
               <img
-                src="https://picsum.photos/id/64/200/300"
+                src={personalInfo.image}
                 className="rounded-full w-[80px] h-[80px] object-cover p-1"
-                alt="user_image"
+                alt={personalInfo.speciality}
               />
             </div>
 
@@ -117,7 +150,7 @@ function MasterSettings() {
             </div>
           </div>
 
-          <div className="grid grid-cols-2 border-[2px] border-custom-green-60 rounded-[5px] gap-4 mt-[25px] p-3 items-center">
+          <div className="grid grid-cols-2 border-[2px] border-custom-green-15 rounded-[5px] gap-4 mt-[25px] p-3 items-center">
             <div className="col-span-2 text-[18px] font-bold grid grid-cols-2">
               <p>Personal Information</p>
               <div className="flex justify-end">
@@ -129,44 +162,42 @@ function MasterSettings() {
               <span className="font-semibold text-[14px] opacity-60">
                 First name
               </span>
-              <p className="font-semibold text-[18px]">Zerda</p>
+              <p className="font-semibold text-[18px]">{personalInfo.first_name}</p>
             </div>
 
             <div className="px-6">
               <span className="font-semibold text-[14px] opacity-60">
                 Last name
               </span>
-              <p className="font-semibold text-[18px]">Jursinova</p>
+              <p className="font-semibold text-[18px]">{personalInfo.last_name}</p>
             </div>
 
             <div className="px-6">
               <span className="font-semibold text-[14px] opacity-60">
                 Phone number:
               </span>
-              <p className="font-semibold text-[18px]">+998 (97) 123 45 67</p>
+              <p className="font-semibold text-[18px]">{personalInfo.phone_number}</p>
             </div>
 
             <div className="px-6">
               <span className="font-semibold text-[14px] opacity-60">
                 Email Address
               </span>
-              <p className="font-semibold text-[18px]">
-                jursinova@gmail.com
-              </p>
+              <p className="font-semibold text-[18px]">{personalInfo.email}</p>
             </div>
 
             <div className="px-6">
               <span className="font-semibold text-[14px] opacity-60">
                 Specialist
               </span>
-              <p className="font-semibold text-[18px]">Coder</p>
+              <p className="font-semibold text-[18px]">{personalInfo.speciality}</p>
             </div>
 
             <div className="px-6">
               <span className="font-semibold text-[14px] opacity-60">
                 Job time
               </span>
-              <p className="font-semibold text-[18px]">Part-Time</p>
+              <p className="font-semibold text-[18px]">{ personalInfo.work_type === "part_time" ? "PART-TIME" : "FULL-TIME" }</p>
             </div>
 
             <div className="px-6">
@@ -263,15 +294,13 @@ function MasterSettings() {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-[25px] gap-4">
-
           <div className="border-[2px] border-custom-green-60 rounded-[5px]">
             <div className="flex p-4">
               <p className="justify-start font-bold text-[24px] w-full">
                 BankName Card 1
               </p>
-                        
+
               <EditCardInfo />
-              
             </div>
 
             <div className="flex p-4 h-[150px] items-end">
@@ -287,13 +316,9 @@ function MasterSettings() {
             </div>
           </div>
 
-
           <div className="border-[2px] border-custom-green-60 rounded-[5px] flex justify-center items-center">
             <AddCard />
           </div>
-
-         
-
         </div>
       </div>
       {/* My Payment END */}
