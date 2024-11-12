79c190ad6488271b1df1e57135068767c4338e34
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-12 14:47:09 +0500

    Create new project button worked

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -129,8 +129,8 @@ const Line = () => {
           setFormData({
             projectName: "",
             projectDeadline: "",
-          })
-          document.getElementById("new_project").close()
+          });
+          document.getElementById("new_project").close();
           return <b>Add :)</b>;
         },
         error: (error) => {
@@ -189,9 +189,36 @@ const Line = () => {
 
   const formattedDate = new Date().toLocaleDateString();
 
-  const submitSelectedUsers = (e) =>{
+  const submitSelectedUsers = (e) => {
+    e.preventDefault();
+    toast.promise(
+      http.patch(
+        `projects/${activeProject.id}/`,
+        {
+          members: selectedUsers,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access")}`,
+          },
+        }
+      ),
+      {
+        loading: "Adding ...",
+        success: (response) => {
+          console.log(response.data);
+          getProjectsList();
+          document.getElementById("adduser").close();
+          return <b>Add :)</b>;
+        },
+        error: (error) => {
+          console.log(error.response.data);
 
-  }
+          return <b>Error :(</b>;
+        },
+      }
+    );
+  };
 
   return (
     <>
@@ -299,7 +326,7 @@ const Line = () => {
           </button>
         </div>
 
-        {/* Add User Modal */}
+        {/* Add User Modal for Project */}
         <dialog id="adduser" className="modal">
           <Toaster />
           <div className="modal-box p-0">
@@ -319,7 +346,7 @@ const Line = () => {
             </form>
             {/* Modal header End */}
 
-            <form action="">
+            <form action="" onSubmit={submitSelectedUsers}>
               <div className="p-4">
                 {membersInfo.map((user) => (
                   <div
@@ -357,7 +384,10 @@ const Line = () => {
                 ))}
               </div>
               <div className="px-4 pb-4">
-                <button ty className="w-full btn bg-custom-green-15 text-custom-green-dark border-transparent hover:text-white hover:bg-custom-green-dark transition-all duration-300">
+                <button
+                  type="submit"
+                  className="w-full btn bg-custom-green-15 text-custom-green-dark border-transparent hover:text-white hover:bg-custom-green-dark transition-all duration-300"
+                >
                   Save
                 </button>
               </div>
@@ -390,39 +420,27 @@ const Line = () => {
 
             <div className="p-3">
               {activeProject.members && activeProject.members.length !== 0 ? (
-                activeProject.members.map((userId, index) => {
-                  // Find the member info by matching the id
-                  const user = membersInfo.find((m) => m.id === userId);
-
-                  return user ? (
-                    <div
-                      key={index}
-                      className={`${
-                        selectedUsers.includes(user.id)
-                          ? "bg-white"
-                          : "bg-transparent"
-                      }`}
-                    >
-                      <label className="label">
-                        <div className="flex">
-                          {/* Display user image */}
-                          <img
-                            src={user.image || ""}
-                            alt={`${user.first_name} ${user.last_name}`}
-                            className="w-[45px] h-[45px] object-cover rounded-full"
-                          />
-                          <div className="ml-4">
-                            {/* Display user first and last name */}
-                            <p className="font-bold text-custom-green-dark">
-                              {user.first_name} {user.last_name}
-                            </p>
-                            {/* Display user label */}
-                            <p className="text-custom-green-60">{user.label}</p>
-                          </div>
+                activeProject.members.map((member) => {
+                  <div key={member.id} className="bg-white">
+                    <label className="label">
+                      <div className="flex">
+                        {/* Display user image */}
+                        <img
+                          src={member.image}
+                          alt={`${member.full_name}`}
+                          className="w-[45px] h-[45px] object-cover rounded-full"
+                        />
+                        <div className="ml-4">
+                          {/* Display user first and last name */}
+                          <p className="font-bold text-custom-green-dark">
+                            {member.full_name}
+                          </p>
+                          {/* Display user label */}
+                          <p className="text-custom-green-60">{user.label}</p>
                         </div>
-                      </label>
-                    </div>
-                  ) : null; // If user not found, return null
+                      </div>
+                    </label>
+                  </div>;
                 })
               ) : (
                 <div className="grid grid-cols-1 text-center p-10 text-custom-green-80 select-none">
@@ -442,7 +460,6 @@ const Line = () => {
 
       {/* New Project Modal */}
       <dialog id="new_project" className="modal text-custom-green-dark">
-
         <Toaster />
 
         <div className="modal-box p-0">
