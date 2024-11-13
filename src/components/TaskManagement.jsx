1f16ee4d032bf2df56ffc0fc4d3166497a2b3419
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-13 10:27:54 +0500

    clean code Task managment

--- src/components/TaskManagement.jsx
+++ src/components/TaskManagement.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { Board } from "../data/board";
 import { onDragEnd } from "../helpers/onDragEnd";
 import AddTaskModal from "../components/AddTaskModal";
-import Avatar from "./Avatar"
+import Avatar from "./Avatar";
 import Days from "./Days";
 import Line from "./Line";
 
@@ -28,37 +28,33 @@ function TaskManagement() {
 
   return (
     <>
+      <Line />
 
-    <Line />
-    
       <DragDropContext
-
         onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
       >
-        
-
         {/* <div className="flex items-start px-4 bg-custom-green-10 rounded-[15px]  space-x-4 border border-red-700">  */}
-        <div className="grid grid-cols-4 px-4 bg-custom-green-10 rounded-[15px] gap-4"> 
+        <div className="grid grid-cols-4 px-4 bg-custom-green-10 rounded-[15px] gap-4">
           {Object.entries(columns).map(([columnId, column], index) => (
-
             <div className="flex flex-col gap-2" key={columnId}>
-              <Droppable droppableId={columnId} key={columnId} >
+              <Droppable droppableId={columnId} key={columnId}>
                 {(provided) => (
                   <div
                     ref={provided.innerRef}
                     {...provided.droppableProps}
                     className="grid grid-col gap-3 items-center py-4 w-full" //w-[250px] md:w-[300px] lg:w-[320px] xl:w-[350px] 2xl:w-[400px]
                   >
-
                     <div className="flex justify-between p-2 py-[10px] w-full bg-white rounded-lg shadow-sm text-custom-green-dark  text-[15px] font-extrabold">
                       {column.name}
-                      <div className="bg-custom-green-30 w-5 text-center  text-custom-green-dark rounded ">3</div> 
+                      <div className="bg-custom-green-30 w-5 text-center  text-custom-green-dark rounded ">
+                        3
+                      </div>
                     </div>
 
                     {index === 0 && (
                       <div
                         onClick={() => openModal(columnId)}
-                        //w-[250px] md:w-[300px] lg:w-[320px] xl:w-[350px] 2xl:w-[400px]  
+                        //w-[250px] md:w-[300px] lg:w-[320px] xl:w-[350px] 2xl:w-[400px]
                         className="flex cursor-pointer items-center justify-center gap-1 py-[10px] w-full opacity-90 bg-white rounded-lg shadow-sm text-[#555] font-medium text-[15px]"
                       >
                         <i className="bi bi-plus-circle-dotted"></i>
@@ -81,9 +77,7 @@ function TaskManagement() {
                               {...provided.dragHandleProps}
                               className="w-full cursor-grab bg-[#fff] flex flex-col justify-between gap-3 items-start shadow-sm rounded-xl px-3 py-4"
                             >
-                         
-                                <Days />
-                
+                              <Days />
 
                               {task.image && task.alt && (
                                 <img
@@ -116,7 +110,6 @@ function TaskManagement() {
                               </div>
                               <div className="w-full border border-dashed"></div>
                               <div className="w-full flex items-center justify-between">
-
                                 <div className="flex items-center gap-1">
                                   <i className="bi bi-clock"></i>
                                   <span className="text-[13px] text-gray-700">
@@ -124,14 +117,14 @@ function TaskManagement() {
                                   </span>
                                 </div>
 
-
                                 <div
-                                  className={`w-[60px] rounded-full h-[5px] ${task.priority === "high"
+                                  className={`w-[60px] rounded-full h-[5px] ${
+                                    task.priority === "high"
                                       ? "bg-red-500"
                                       : task.priority === "medium"
-                                        ? "bg-orange-500"
-                                        : "bg-blue-500"
-                                    }`}
+                                      ? "bg-orange-500"
+                                      : "bg-blue-500"
+                                  }`}
                                 ></div>
                               </div>
                               <div className="w-full border border-dashed"></div>
@@ -146,16 +139,13 @@ function TaskManagement() {
                       </Draggable>
                     ))}
                     {provided.placeholder}
-
                   </div>
                 )}
               </Droppable>
             </div>
           ))}
         </div>
-
       </DragDropContext>
-
 
       <AddTaskModal
         isOpen={modalOpen}
