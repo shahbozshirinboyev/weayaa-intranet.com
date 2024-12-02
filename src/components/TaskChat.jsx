81d9a56df0cd6e29c3733f252f05c82cfecfcfa3
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-12-02 15:19:49 +0500

    TaskChat Process

--- src/components/TaskChat.jsx
+++ src/components/TaskChat.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import { useState } from "react";
 
 function TaskChat({ task }) {
+  const showTaskInfo = () => {
+    console.log(task);
+  };
+
+  const [fileName, setFileName] = useState("");
+
+  const handleFileChange = (event) => {
+    if (event.target.files.length > 0) {
+      setFileName(event.target.files[0].name); // Tanlangan fayl nomi
+    } else {
+      setFileName("Hech qanday fayl tanlanmagan");
+    }
+  };
+
   return (
     <>
       <button
-        onClick={() =>
-          document.getElementById(`task_chat${task.id}`).showModal()
-        }
+        onClick={() => {
+          document.getElementById(`task_chat${task.id}`).showModal();
+          showTaskInfo();
+        }}
       >
         <div className="flex">
           <div className="relative">
@@ -20,11 +35,11 @@ function TaskChat({ task }) {
 
       {/* TASK => Chat START */}
       <dialog id={`task_chat${task.id}`} className="modal">
-        <div className="modal-box h-full max-h-[700px] p-0 flex flex-col">
+        <div className="modal-box h-full max-h-[700px] p-0 flex flex-col rounded-none">
           {/* Modal header Start */}
           <form
             method="dialog"
-            className="border-b-[2px] border-custom-green-80 h-[60px] grid grid-cols-2 items-center px-[24px] bg-custom-green-10"
+            className="border-b-[2px] w-full sticky top-0 z-[999] border-custom-green-80 min-h-[60px] grid grid-cols-2 items-center px-[24px] bg-white"
           >
             <span className="text-custom-green-dark font-bold">Task Chat</span>
             <div className="text-end">
@@ -32,11 +47,199 @@ function TaskChat({ task }) {
                 <i className="bi bi-x-lg flex justify-center items-center"></i>
               </button>
             </div>
+
+            <div className="min-w-full h-hull absolute h-[60px] bg-custom-green-10"></div>
           </form>
           {/* Modal header End */}
           {/* Task Chat Body START */}
-          <div className="border">Task Chat</div>
+          <section className="px-4 overflow-auto chatcss">
+            <div className="chat chat-start">
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS chat bubble component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <div className="chat-header">
+                Obi-Wan Kenobi
+                <time className="text-xs opacity-50">12:45</time>
+              </div>
+              <div className="chat-bubble">You were the Chosen One!</div>
+              <div className="chat-footer opacity-50">Delivered</div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS chat bubble component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <div className="chat-header">
+                Obi-Wan Kenobi
+                <time className="text-xs opacity-50">12:45</time>
+              </div>
+              <div className="chat-bubble">You were the Chosen One!</div>
+              <div className="chat-footer opacity-50">Delivered</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS chat bubble component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <div className="chat-header">
+                Anakin
+                <time className="text-xs opacity-50">12:46</time>
+              </div>
+              <div className="chat-bubble">I hate you!</div>
+              <div className="chat-footer opacity-50">Seen at 12:46</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS chat bubble component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <div className="chat-header">
+                Anakin
+                <time className="text-xs opacity-50">12:46</time>
+              </div>
+              <div className="chat-bubble">I hate you!</div>
+              <div className="chat-footer opacity-50">Seen at 12:46</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS chat bubble component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <div className="chat-header">
+                Anakin
+                <time className="text-xs opacity-50">12:46</time>
+              </div>
+              <div className="chat-bubble">I hate you!</div>
+              <div className="chat-footer opacity-50">Seen at 12:46</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS chat bubble component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <div className="chat-header">
+                Anakin
+                <time className="text-xs opacity-50">12:46</time>
+              </div>
+              <div className="chat-bubble">I hate you!</div>
+              <div className="chat-footer opacity-50">Seen at 12:46</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS chat bubble component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <div className="chat-header">
+                Anakin
+                <time className="text-xs opacity-50">12:46</time>
+              </div>
+              <div className="chat-bubble">I hate you!</div>
+              <div className="chat-footer opacity-50">Seen at 12:46</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS chat bubble component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <div className="chat-header">
+                Anakin
+                <time className="text-xs opacity-50">12:46</time>
+              </div>
+              <div className="chat-bubble">I hate you!</div>
+              <div className="chat-footer opacity-50">Seen at 12:46</div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS chat bubble component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <div className="chat-header">
+                Obi-Wan Kenobi
+                <time className="text-xs opacity-50">12:45</time>
+              </div>
+              <div className="chat-bubble">You were the Chosen One!</div>
+              <div className="chat-footer opacity-50">Delivered</div>
+            </div>
+          </section>
           {/* Task Chat Body END */}
+          {/* Chat Input START */}
+
+          <div
+            className={`flex justify-between items-center px-3 py-1 ${
+              fileName === "" ? "hidden" : ""
+            } bg-custom-green-15`}
+          >
+            <span className="text-gray-700">{fileName}</span>
+            <button className="btn btn-sm">
+              <i className="bi bi-x flex justify-center items-center"></i>
+            </button>
+          </div>
+
+          <div className="h-[60px] py-2 px-3">
+            <form action="" className="flex gap-2">
+              <div className="flex items-center gap-4">
+                <label
+                  htmlFor="fileInput"
+                  className="px-2 py-1 border cursor-pointer"
+                >
+                  <i className="bi bi-paperclip flex justify-center items-center text-[20px]"></i>
+                </label>
+                <input
+                  type="file"
+                  id="fileInput"
+                  className="hidden"
+                  onChange={handleFileChange} // Faylni tanlashda ishlaydi
+                />
+              </div>
+
+              <input
+                type="text"
+                className="border w-full px-2 py-1 outline-none text-custom-green-dark"
+                placeholder="Write a message..."
+              />
+              <button className="px-2 py-1 border cursor-pointer">
+                <i className="bi bi-send flex justify-center items-center  text-[20px]"></i>
+              </button>
+            </form>
+          </div>
+          {/* Chat Input END */}
         </div>
         <form method="dialog" className="modal-backdrop">
           <button>close</button>
