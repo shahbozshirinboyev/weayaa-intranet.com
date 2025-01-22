fc1eb13231b35e0008cbfc65f802689f2441cb1a
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-01-22 19:41:33 +0500

    Client page fix some bugs

--- src/pages/Customer/ProjectsList.jsx
+++ src/pages/Customer/ProjectsList.jsx
@@ -31,14 +31,14 @@ function ProjectsList() {
     <div className="w-full flex flex-col">
       {/* Map Project List Start */}
       {!loading &&
-        projectsList?.map((project) => (
+        projectsList?.sort((a, b) => new Date(a.created_at) - new Date(b.created_at)).map((project) => (
           <div
             key={project.id}
             className="bg-custom-green-5 w-full h-[60px] rounded-[5px] flex mb-4"
           >
             <div className="p-2 h-full">
               <div className="flex h-full justify-start px-4 w-[300px] items-center bg-custom-green-30 text-custom-green-dark font-semibold rounded-[5px]">
-                <i className="bi bi-folder mr-4 text-[18px] flex justify-center items-center"></i>
+                <i className="bi bi-folder mr-3 text-[18px] flex justify-center items-center"></i>
                 <p className="whitespace-nowrap overflow-hidden text-ellipsis">
                   {project.name}
                 </p>
@@ -46,18 +46,20 @@ function ProjectsList() {
             </div>
 
             <div className="p-2 h-full hidden md:flex">
-              <div className="flex h-full justify-start px-4 w-[270px]  items-center bg-custom-green-30 text-custom-green-dark font-semibold rounded-[5px]">
-                <i className="bi bi-calendar4-week mr-4 text-[18px] flex justify-center items-center"></i>
+              <div className="flex h-full px-4 w-[275px]  items-center justify-between bg-custom-green-30 text-custom-green-dark font-semibold rounded-[5px]">
+                <div className="flex">
+                <i className="bi bi-calendar4-week mr-3 text-[18px] flex justify-center items-center"></i>
                 <p>{project.deadline}</p>
-                <p className={`flex justify-center items-center whitespace-nowrap ml-4 px-2 py-1 rounded-[5px] w-[100px] ${ Math.ceil( (new Date(project.deadline) - new Date()) / (1000 * 60 * 60 * 24) ) < 0 ? "text-white bg-red-700" : "text-white bg-custom-green-dark"}`} >
+                </div>
+                <p className={`flex justify-center items-center whitespace-nowrap px-2 py-1 rounded-[5px] w-[105px] ${ Math.ceil( (new Date(project.deadline) - new Date()) / (1000 * 60 * 60 * 24) ) < 0 ? "text-white bg-red-700" : "text-white bg-custom-green-dark"}`} >
                   {Math.ceil( (new Date(project.deadline) - new Date()) / (1000 * 60 * 60 * 24) )}{" "}days
                 </p>
               </div>
             </div>
 
             <div className="p-2 h-full hidden lg:flex">
-              <div className="flex h-full justify-start px-4 w-[180px] items-center bg-custom-green-30 text-custom-green-dark font-semibold rounded-[5px]">
-                <i className="bi bi-people mr-4 text-[18px] flex justify-center items-center"></i>
+              <div className="flex h-full justify-start px-4 w-[195px] items-center bg-custom-green-30 text-custom-green-dark font-semibold rounded-[5px]">
+                <i className="bi bi-people mr-3 text-[18px] flex justify-center items-center"></i>
                 <p>{project.members.length} Staff working</p>
               </div>
             </div>
